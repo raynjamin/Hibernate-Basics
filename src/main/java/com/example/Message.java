3e07703c78e6e443988dc39d37a7262e6602c912
Emileen <emileen.13@gmail.com>
2017-01-18 17:10:23 -0500

    added message

---
+++ src/main/java/com/example/Message.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import javax.persistence.*;
+
+/**
+ * Created by emileenmarianayagam on 1/18/17.
+ */
+@Entity
+@Table (name ="messages")
+public class Message {
+    @Id
+    @GeneratedValue
+    int id;
+
+    @Column(nullable = false)
+    String message;
+
+    public Message() {
+    }
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+}
---
+++ src/main/java/com/example/MessageController.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * Created by emileenmarianayagam on 1/18/17.
+ */
+@Controller
+public class MessageController {
+    @Autowired
+    MessageRepository messages;
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String home(Model model) {
+        List<Message> messageList = (List)messages.findAll();
+        model.addAttribute("messages", messageList);
+        return "home";
+    }
+
+    @RequestMapping(path = "/add-message", method = RequestMethod.POST)
+    public String addMessage (String message){
+        /*System.out.println("The message is:");
+        System.out.println(message);*/
+        Message newMessage = new Message(message);
+        messages.save(newMessage);
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/delete-message", method = RequestMethod.GET)
+    public String deleteMessage(int id){
+
+        return "home";
+    }
+
+}
